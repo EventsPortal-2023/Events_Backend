d76482f749f2574f81e72fefd0ecc8be4c60e293
Dhruvin3103 <dcchawda2003@gmail.com>
2023-04-30 19:15:55 +0530

    added update and delete view also done filtering for events

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from models import *
+from .models import *
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('Events/', views.EventList.as_view(), name="Events"),
-    path('Likes/', views.EventLikesList.as_view(), name = "Author"), 
+    path('Events/<int:pk>', views.Eventmixin.as_view(), name="Events"),
+    path('Eventsearch/', views.EventListSearch.as_view(), name="Events"),
+    path('Likes/', views.EventLikesList.as_view(), name = "Author"),
+    path('photos/<int:pk>', views.Photomixin.as_view(), name="Events"),
+    path('Likes/<int:pk>', views.EventLikesmixin.as_view(), name="Author"),
 ]

17d61e94aacdde63d2fb2f9dccbcca6cdf77bf2c
Dhruvin3103 <dcchawda2003@gmail.com>
2023-07-19 17:20:31 +0530

    update

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+import os
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -136,6 +136,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = '/home/rihansh113/Events_Backend/static'
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
